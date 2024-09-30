184d7c76d22f5687b3d934b89b27734cde10beb3
Joey <joey@Joeys-MacBook-Pro.local>
2024-09-30 10:41:55 +0200

    Change sms proxy to debug

--- src/components/Access.js
+++ src/components/Access.js
@@ -27,7 +27,7 @@ import "react-toastify/dist/ReactToastify.css";
   ) => {
     const result = getAccount();
     const provider = await result.connector?.getProvider();
-    const dataProtector = new IExecDataProtector(provider);
+    const dataProtector = new IExecDataProtector(provider, {iexecOptions: { smsURL: 'https://sms.scone-debug.v8-bellecour.iex.ec' }});
     const accessHash = await dataProtector.grantAccess({
       protectedData,
       authorizedUser,
